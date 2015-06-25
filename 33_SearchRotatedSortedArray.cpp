a85fd1eea1a34981ef2279c6595a5fc98798d1f2
maybeluo <csluoyf@163.com>
2015-06-26 01:11:09 +0800

    Update 33_SearchRotatedSortedArray.cpp

--- 33_SearchRotatedSortedArray.cpp
+++ 33_SearchRotatedSortedArray.cpp
@@ -1,10 +1,11 @@
 // Beautiful! Find the pivot first, then binary search two sorted part.
 // The process of finding the pivot is rather fragile.
 // time complexity: O(log n)
-// WA 3 times:
-//  1) L16: should be "l<=r", NOT "l<r". Otherwise, the situation of nums[l] (=nums[r]) isn't processed.
-//  2) L42: ">=0", NOT ">0".
-//  3) L32: ">=", NOT ">". This really makes a difference. Think it over! Take [1,3] and [3,1] for example.
+// WA 4 times:
+//  1) L33: compare with "nums[0]", NOT "nums[l]"!
+//  2) L33: ">=", NOT ">". This really makes a difference. Think it over! Take [1,3] and [3,1] for example.
+//  3) L17: "l<=r", NOT "l<r". Otherwise, the situation of nums[l] (=nums[r]) isn't processed.
+//  4) L43: ">=0", NOT ">0".
 
 #include <iostream>
 #include <vector>
