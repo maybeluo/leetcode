fd9e89ca6ac91dafeeab672a30c217c980dd6b8f
maybeluo <csluoyf@163.com>
2015-06-30 09:40:37 +0800

    Create 78_Subsets.cpp

---
+++ 78_Subsets.cpp
@@ -0,0 +1,68 @@
+// Brute force: use <bitset> to enumerate.
+
+#include <iostream>
+#include <bitset>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+const int MAXN = 100;
+
+void dfs(vector< vector<int> >& vv, vector<int>& nums, bitset<MAXN>& bt, int n, int pos) {
+    if(pos >= n ){
+        string ans = bt.to_string();
+        vector<int> vt;
+        vt.clear();
+        for(int i = 0; i < n; i++) {
+            if(ans[i] == '1') {
+                vt.push_back(nums[i]);
+                cout<<nums[i]<<" ";
+            }
+        }
+        cout<<endl;
+        vv.push_back(vt);
+        return ;
+    }
+    bt.flip(MAXN - pos - 1);
+    dfs(vv, nums, bt, n, pos + 1);
+    bt.flip(MAXN - pos - 1);
+    dfs(vv, nums, bt, n, pos + 1);
+}
+
+vector< vector<int> > subsets(vector<int>& nums) {
+    vector< vector<int> > vv;
+    vv.clear();
+    int n = nums.size();
+    if(n < 1) return vv;
+
+    sort(nums.begin(), nums.end());
+    int i = 0, j = 0;
+    for(i = 1; i < n; i++) {
+        if(nums[i] != nums[j]) {
+            j ++;
+            nums[j] = nums[i];
+        }
+    }
+    int nu = j + 1;
+    cout<<nu<<endl;
+    bitset<MAXN> bt;
+    bt.reset();
+    dfs(vv, nums, bt, nu, 0);
+    return vv;
+}
+
+int main()
+{
+    int n;
+    while(cin>>n){
+        int t;
+        vector<int> v;
+        v.clear();
+        for(int i = 0; i < n; i++){
+            cin>> t;
+            v.push_back(t);
+        }
+        subsets(v);
+    }
+    return 0;
+}
