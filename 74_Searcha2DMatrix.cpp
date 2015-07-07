de609689f42b45fb3c5c0f2d6e14bdd3b463778b
maybeluo <csluoyf@163.com>
2015-07-07 14:06:07 +0800

    Create 74_Searcha2DMatrix.cpp

---
+++ 74_Searcha2DMatrix.cpp
@@ -0,0 +1,24 @@
+//Diveide and Conquer.
+
+class Solution {
+public:
+    bool dc(vector<vector<int>>& matrix, int x1, int y1, int x2, int y2, int target) {
+        if(x1 > x2 || y1 > y2) return false;
+        int midx = (x1 + x2) >> 1, midy = (y1 + y2) >> 1;
+        if( matrix[midx][midy] == target ) return true;
+        else if( matrix[midx][midy] < target ) {
+            return dc(matrix, x1, midy + 1, midx, y2, target) || dc(matrix, midx + 1, y1, x2, y2, target);
+        }
+        else {
+            return dc(matrix, x1, y1, x2, midy - 1, target) || dc(matrix, x1, midy, midx - 1, y2, target);
+        }
+    }
+    
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m = matrix.size();
+        if(m < 1) return false;
+        
+        int n = matrix[0].size();
+        return dc(matrix, 0, 0, m - 1, n - 1, target);
+    }
+};
