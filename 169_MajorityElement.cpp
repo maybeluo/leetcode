c88e65e8d989452c108fa179d7e15831448c8192
maybeluo <csluoyf@163.com>
2015-07-21 08:34:49 +0800

    Update 169_MajorityElement.cpp

--- 169_MajorityElement.cpp
+++ 169_MajorityElement.cpp
@@ -16,3 +16,55 @@ public:
         return ans;
     }
 };
+
+// counting: one counter
+//  You can assume that there are three "number" in the vector. Teo is the majority number, the other one is the rest of the vector.
+int majorityElement(vector<int>& nums) {
+    int n = nums.size();
+    if(n < 1) return -1;
+    
+    int a, ia = 0;
+    for(int i = 0; i < n; i++) {
+        if(ia == 0) {
+            a = nums[i];
+            ia = 1;
+        }
+        else if(nums[i] == a) {
+            ia ++;
+        }
+        else {
+            ia --;
+        }
+    }
+    return a;
+}
+
+// two counters.
+int majorityElement(vector<int>& nums) {
+    int n = nums.size();
+    if(n < 1) return -1;
+    
+    int a, b, ia, ib = 0;
+    a = nums[0], ia = 1;
+    for(int i = 1; i < n; i++) {
+        if(nums[i] == a) ia ++;
+        else if(nums[i] != a && ib < 1) {//b hasn't been assigned a value
+            b = nums[i];
+            ib = 1;
+        }
+        else if(ib >= 1 && nums[i] == b) ib ++;
+        else if( nums[i] != a && (ib >= 1 && nums[i] != b) ) {
+            if(ia > ib) {
+                b = nums[i];
+                ib = 1;
+            }
+            else{
+                a = nums[i];
+                ia = 1;
+            }
+        }
+    }
+    
+    if(ia > ib) return a;
+    else return b;
+}
