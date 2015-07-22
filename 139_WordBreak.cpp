d41212c29d5ad18fc38cf08e50b7daf44c8af9d7
maybeluo <csluoyf@163.com>
2015-07-23 00:43:25 +0800

    Update 139_WordBreak.cpp

--- 139_WordBreak.cpp
+++ 139_WordBreak.cpp
@@ -25,3 +25,34 @@ public:
         else return false;
     }
 };
+
+// version 2: use a set to store the valid split position.
+class Solution {
+public:
+    bool wordBreak(string s, unordered_set<string>& wordDict) {
+        if(s.size() < 1) return false;
+        
+        int n = s.size();
+        vector<int> dp(n + 1, 0); // dp[i]: whether string from [0, i) can get valid break
+        
+        unordered_set<int> able;
+        able.clear();
+        for(int i = 0; i < n; i++) {
+            if( wordDict.find( s.substr(0, i + 1) ) != wordDict.end()  ) {
+                dp[i + 1] = 1;
+                able.insert(i + 1);
+            }
+            else {
+                for(unordered_set<int>::iterator it = able.begin(); it != able.end(); it++) {
+                    if(dp[*it] && wordDict.find( s.substr(*it, i - *it + 1) ) != wordDict.end() ) {
+                        dp[i + 1] = 1;
+                        able.insert(i+1);
+                        break; // only need to find a valid solution
+                    }
+                }
+            }
+        }
+        if(dp[n]) return true;
+        else return false;
+    }
+};
