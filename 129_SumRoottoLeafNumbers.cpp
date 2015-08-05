b9e3d66f8257b23623b1e39660ed72423c870fda
maybeluo <csluoyf@163.com>
2015-08-05 23:02:36 +0800

    Update 129_SumRoottoLeafNumbers.cpp

--- 129_SumRoottoLeafNumbers.cpp
+++ 129_SumRoottoLeafNumbers.cpp
@@ -33,3 +33,20 @@ public:
         return ans;
     }
 };
+
+// version 2.
+void dfs(TreeNode* root, int cnt, int& sum) {
+	if(root == NULL) return ;
+    if(root -> left == NULL && root -> right == NULL) {
+    	sum += ( cnt *10 + root -> val);
+    	return ;
+    }
+    dfs(root -> left, cnt *10 + root -> val, sum);
+    dfs(root -> right, cnt *10 + root -> val, sum);
+}
+
+int sumNumbers(TreeNode *root) {
+	int sum = 0;
+	dfs(root, 0, sum);
+	return sum;
+}
