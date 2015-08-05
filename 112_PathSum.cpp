7306413e782763675893a6024b23358ed1246920
maybeluo <csluoyf@163.com>
2015-08-05 21:02:24 +0800

    Update 112_PathSum.cpp

--- 112_PathSum.cpp
+++ 112_PathSum.cpp
@@ -11,32 +11,43 @@
  */
 class Solution {
 public:
-    void dfs(int& freq, vector<int> vt, TreeNode* nt, int cnt, int sum) {
+    void dfs(int& freq, TreeNode* nt, int cnt, int sum) {
         if(nt->left == NULL && nt->right == NULL){
             if(cnt + nt->val == sum){
                 freq += 1;
             }
             return ;
         }
-        vt.push_back(nt->val);
         if(nt->left != NULL){
-            dfs(freq, vt, nt->left, cnt + nt->val, sum);
+            dfs(freq, nt->left, cnt + nt->val, sum);
         }
         if(nt->right != NULL){
-            dfs(freq, vt, nt->right, cnt + nt->val, sum);
+            dfs(freq, nt->right, cnt + nt->val, sum);
         }
-        vt.pop_back();
     }
     
     bool hasPathSum(TreeNode* root, int sum) {
         int freq = 0;
         
         if(root == NULL) return freq;
-        
-        vector<int> vt;
-        vt.clear();
-        dfs(freq, vt, root, 0, sum);
+
+        dfs(freq, root, 0, sum);
 
         return freq;
     }
 };
+
+// version 2.
+bool dfs(TreeNode* root, int cnt, int target) {
+    if(root == NULL) return false;
+    if(root -> left == NULL && root -> right == NULL) {
+        if(cnt + root -> val == target) return true;
+        else return false;
+    }
+    return dfs(root -> left, cnt + root -> val, target)
+        || dfs(root -> right, cnt + root -> val, target);
+}
+
+bool hasPathSum(TreeNode* root, int sum) {
+    return dfs(root, 0, sum);
+}
