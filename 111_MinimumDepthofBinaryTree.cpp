8061d9b3430e672188e7495948b0f4d178228cff
maybeluo <csluoyf@163.com>
2015-08-05 20:35:03 +0800

    Update 111_MinimumDepthofBinaryTree.cpp

--- 111_MinimumDepthofBinaryTree.cpp
+++ 111_MinimumDepthofBinaryTree.cpp
@@ -28,5 +28,32 @@ public:
 
         return mx;
     }
-
 };
+
+// version 2
+int minDepth(TreeNode* root) {
+    if(root == NULL) return 0;
+    if(root -> left == NULL && root -> right == NULL) return 1;
+    else if(root->left == NULL) return 1 + minDepth(root -> right);
+    else if(root->right == NULL) return 1 + minDepth(root -> left);
+    else return 1 + min(minDepth(root -> left), minDepth(root -> right));
+}
+
+// version 3
+int minDepth(TreeNode* root) {
+    int depth = 0x3fffffff;
+    stack<pair<TreeNode*, int>> s;
+    s.push( make_pair(root, 1) );
+    while( !s.empty() ) {
+        pair<TreeNode*, int> p = s.top();
+        s.pop();
+        if(p.first == NULL) continue;
+        if(p.first -> left == NULL && p.first -> right == NULL) depth = min(depth, p.second);
+        else {
+            s.push( make_pair(p.first -> left, p.second + 1) );
+            s.push( make_pair(p.first -> right, p.second + 1) );
+        }
+    }
+    if(depth >= 0x3fffffff) return 0;
+    return depth;
+}
