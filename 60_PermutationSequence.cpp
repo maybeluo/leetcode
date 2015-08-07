981bbe2ea7a76438072f8f03f3319ced22cd8186
maybeluo <csluoyf@163.com>
2015-08-07 20:51:49 +0800

    Create 60_PermutationSequence.cpp

---
+++ 60_PermutationSequence.cpp
@@ -0,0 +1,16 @@
+string getPermutation(int n, int k) {
+	vector<int> fact(n + 1, 1);
+	for(int i = 1; i <= n; i++) fact[i] = fact[i-1] * i;
+	vector<char> nums(n);
+	for(int i = 0; i < n; i++) nums[i] = (i + 1 + '0');
+	string ans = "";
+	k --;
+	while(n > 0) {
+		n--;
+		int ind = k/fact[n];
+		ans += nums[ind];
+		nums.erase(nums.begin() + ind);
+		k %= fact[n];
+	}
+	return ans;
+}
