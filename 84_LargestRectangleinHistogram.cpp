3497fe69bbd66f3cfbf5f39bd8b783a178b91832
maybeluo <csluoyf@163.com>
2015-08-09 10:07:20 +0800

    Update 84_LargestRectangleinHistogram.cpp

--- 84_LargestRectangleinHistogram.cpp
+++ 84_LargestRectangleinHistogram.cpp
@@ -28,3 +28,24 @@ public:
         return mx;
     }
 };
+
+// second version
+int largestRectangleArea(vector<int> & height) {
+	int n = height.size(), mx = 0;
+	int l = -1, r = n, i = 0;
+	vector< pair<int, int> > v;
+	for(i = 0; i < n; i++) {
+		while( !v.empty() && v.back().first > height[i] ) {
+			int left = (v.size() > 1) ? v[v.size() - 2].second : l;
+			mx = max(mx, (i - left - 1) * v.back().first);
+			v.pop_back(); 
+		}
+		v.push_back( make_pair(height[i], i) );
+	}
+	while( !v.empty() ){
+		int left = (v.size() > 1) ? v[v.size() - 1].second : l;
+		mx = max(mx, (r - left - 1)*v.back().first);
+		v.pop_back();
+	}
+	return mx;
+}
