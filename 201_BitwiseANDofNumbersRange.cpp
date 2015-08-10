dbe7a25525981172a223e83fed962b70e25fc193
maybeluo <csluoyf@163.com>
2015-08-10 16:18:36 +0800

    Update 201_BitwiseANDofNumbersRange.cpp

--- 201_BitwiseANDofNumbersRange.cpp
+++ 201_BitwiseANDofNumbersRange.cpp
@@ -28,3 +28,11 @@ public:
         return ans;
     }
 };
+
+// another version
+int rangeBitwiseAnd(int m, int n) {
+    int mask = ~0;
+    while (mask && (m & mask) != (n & mask))
+        mask <<= 1; 
+    return m & mask;
+}
