cae68a95cce5b0fe6c07a03bf1b46c324899742a
maybeluo <csluoyf@163.com>
2015-08-10 11:53:33 +0800

    Update 135_Candy.cpp

--- 135_Candy.cpp
+++ 135_Candy.cpp
@@ -83,3 +83,20 @@ int main() {
 	}
 	return 0;
 }
+
+// a neat version
+int candy(vector<int>& ratings) {
+	int n = ratings.size();
+	
+	vector<int> res(n, 1);
+	for(int i = 1; i < n; i++) {
+		if(ratings[i] > ratings[i-1]) res[i] = res[i-1] + 1;
+	}
+	for(int i = n - 1; i > 0; i--) {
+		if(ratings[i-1] > ratings[i]) res[i-1] = max(res[i-1], res[i] + 1);
+	}
+	
+	int ans = 0;
+	for(int i = 0; i < n; i++) ans += res[i];
+	return ans;
+}
