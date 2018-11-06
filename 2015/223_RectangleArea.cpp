6ba837ef471533485a63a5532206d13768de63df
maybeluo <csluoyf@163.com>
2018-11-06 23:09:52 +0800

    restart@2018

--- 2015/223_RectangleArea.cpp
+++
@@ -1,9 +0,0 @@
-class Solution {
-public:
-    int computeArea(int A, int B, int C, int D, int E, int F, int G, int H) {
-    	int ans = abs(C - A) * abs(D - B) + abs(G - E) * abs(H - F);
-    	int l = max(A, E), r = min(C, G), u = min(D, H), d = max(B, F);
-    	if( l < r && d < u) ans -= (r - l)*(u -  d);
-    	return ans;
-    }
-};
